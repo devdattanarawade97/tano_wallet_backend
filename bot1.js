ba012f575b6bb6c8f27c5edccee293a6bb48e63d
devdatta <devdattanarawade97@gmail.com>
2024-09-02 23:46:21 +0530

    /send functionality added and commands changed to text image and docs

--- bot1.js
+++ bot1.js
@@ -517,9 +517,9 @@ bot.onText(/\/send/, async (msg) => {
      if (sendState[chatId]) {
         const groupTag = msg.text.trim();
 
-        if (!groupTag.startsWith('@')) {
-            bot.sendMessage(chatId, "Invalid group tag. Please ensure it starts with '@'.");
-        } else {
+        // if (!groupTag.startsWith('@')) {
+        //     bot.sendMessage(chatId, "Invalid group tag. Please ensure it starts with '@'.");
+        // } else {
             const chat = await bot.getChat(groupTag);
             const senderId = chat.id;
             console.log("sender id : ", senderId);
@@ -542,6 +542,6 @@ bot.onText(/\/send/, async (msg) => {
 
         // Clear the send state after the message is processed
         sendState[chatId] = false;
-    }
+    // }
 });
 
